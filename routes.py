6a612cca4efd1c327c845543461baa4e2dd3d7de
April Slotterback <april.slotterback@gmail.com>
2020-09-16 18:58:43 -0400

    Add Main Page and links to same

--- routes.py
+++ routes.py
@@ -4,12 +4,15 @@ from models import Boiler, BoilerTube, TubePlugEvent
 
 @app.route('/')
 def index():
-    return "Hello, User!"
+    boiler_ids = db.session.query(Boiler.id).all()
+    return render_template('index.html',boiler_ids=boiler_ids,
+                          subtitle="Select a Boiler")
 
 @app.route('/boiler/<int:id>')
 def boiler(id):
     boiler = Boiler.query.filter_by(id=id).first_or_404()
-    return render_template('boiler_view.html', boiler=boiler)
+    return render_template('boiler_view.html', boiler=boiler,
+                           subtitle="Current Boiler Status")
 
 @app.route('/boiler_plug/<int:id>', methods=['GET', 'POST'])
 def boiler_plug(id):
@@ -22,7 +25,8 @@ def boiler_plug(id):
                 tube.events.append(e)
         db.session.commit()
         return redirect(url_for('boiler',id=id))
-    return render_template('boiler.html',boiler=boiler)
+    return render_template('boiler.html',boiler=boiler,
+                           subtitle="Click on Tubes That Were Plugged")
 
 @app.route('/boiler_repair/<int:id>', methods=['GET', 'POST'])
 def boiler_repair(id):
@@ -34,7 +38,8 @@ def boiler_repair(id):
                 e = TubePlugEvent(is_plug_event = False)
                 tube.events.append(e)
         db.session.commit()
-        return redirect(url_for('boiler',id))
-    return render_template('boiler.html',boiler=boiler)
+        return redirect(url_for('boiler',id=id))
+    return render_template('boiler.html',boiler=boiler,
+                           subtitle="Click on Tubes That Were Replaced")
 
 
